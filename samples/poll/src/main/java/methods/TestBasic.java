f6b05638cfbf29a472270fa605e6150b26c49a87
John L. Singleton <jsinglet@gmail.com>
2013-07-09 15:51:41 -0400

    added test for memory backed sessions, wired up example and added placeholders for writing back persistence

--- samples/poll/src/main/java/methods/TestBasic.java
+++ samples/poll/src/main/java/methods/TestBasic.java
@@ -1,5 +1,7 @@
 package methods;
 
+import pwe.lang.WritableValue;
+
 public class TestBasic {
     public static final void dispatchTest() {
         System.out.println("Hey mother fuckers.");
@@ -17,8 +19,19 @@ public class TestBasic {
         System.out.println("Hey, you passed in the raw Integer: " + arg1);
     }
 
+
     public static final void simpleFunction4(String arg1, Integer arg2, int arg3) {
         System.out.println(String.format("Hey, you passed in the String %s, The Object Integer %d, and the primative integer %d ", arg1, arg2, arg3));
+    }
+
+
+    public static final void simpleFunction5(WritableValue<String> arg1) {
+        System.out.println("Session Bound String Value: " + arg1.getValue());
+
+        // update the value
+
+        arg1.setValue("I was modified in a session.");
+
     }
 
 }
